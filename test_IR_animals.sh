ff05c9cef0c46fe998671572830a07289b87a702
EunSeop <eunseop.lee90@gmail.com>
2018-11-23 21:38:05 +0900

    Remove data of RGB based animal detector

--- test_IR_animals.sh
+++ test_IR_animals.sh
@@ -1,3 +1,7 @@
 ./darknet detector demo cfg/IR_animals.data cfg/IR_animals.cfg IR_animals_backup/IR_animals.backup deer_demo2.mp4
+#./darknet detector demo cfg/1st_mi.data cfg/1st_mi.cfg backup_2nd_result/mi.backup ~/data/meter_test_demo/rect_meter_test1.mp4 -i 0 
+#./darknet detector demo cfg/mi.data cfg/mi.cfg backup/mi.backup -i 0 
+
+# ./darknet detector test cfg/IR_animals.data cfg/IR_animals.cfg IR_animals_backup/IR_animals.backup 00001.png
 # ./darknet detector test cfg/IR_animals.data cfg/IR_animals.cfg IR_animals_backup/IR_animals.backup 00004.jpg
 
